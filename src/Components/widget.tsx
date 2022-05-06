4e6db9fb6fa6342e8c362e8c529e06a709f25028
elisiomualumene <elisiomualumene@gmail.com>
2022-05-06 15:38:25 +0100

    :sparkles: popover

--- src/Components/widget.tsx
+++ src/Components/widget.tsx
@@ -1,14 +1,17 @@
 import { ChatTeardropDots as Chat } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './widgetForm'
 
 
 export const Widget = () => {
 
     return (
-        <Popover className='absolute bottom-5 right-5'>
+        <Popover className='absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end'>
 
             {/* {visibility ? <p>Hello World</p> : null} */}
-            <Popover.Panel>Hello World</Popover.Panel>
+            <Popover.Panel>
+                <WidgetForm/>
+            </Popover.Panel>
 
             <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <Chat className='w-6 h-6'/>
